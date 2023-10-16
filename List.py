5b54b52e999caac8732bc2251d933c45709daf6e
Ardra P <119437597+ardranijesh@users.noreply.github.com>
2023-10-17 00:26:06 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,40 @@
+
+
+'''
+Consider a list (list = []). You can perform the following commands:
+insert i e: Insert integer  at position .
+print: Print the list.
+remove e: Delete the first occurrence of integer .
+append e: Insert integer  at the end of the list.
+sort: Sort the list.
+pop: Pop the last element from the list.
+reverse: Reverse the list.
+Initialize your list and read in the value of  followed by  lines of commands where each command will be of the  types listed above. Iterate through each command in order and perform the corresponding operation on your list.'''
+
+
+
+if __name__ == '__main__':
+ my_list = []
+
+ N = int(input())
+
+ for i in range(N):
+     command = input().split()
+    
+     if command[0] == 'insert':
+        i, e = map(int, command[1:])
+        my_list.insert(i, e)
+     elif command[0] == 'print':
+        print(my_list)
+     elif command[0] == 'remove':
+        e = int(command[1])
+        my_list.remove(e)
+     elif command[0] == 'append':
+        e = int(command[1])
+        my_list.append(e)
+     elif command[0] == 'sort':
+        my_list.sort()
+     elif command[0] == 'pop':
+        my_list.pop()
+     elif command[0] == 'reverse':
+        my_list.reverse()
